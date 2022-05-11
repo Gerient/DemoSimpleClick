d5ffb5ceb0a089ef455a99aa6deee6fd58c2fab4
20014027 <20014027@rp.edu.sg>
2022-05-12 02:04:13 +0800

    completed part 1

--- app/src/main/java/sg/edu/rp/c346/id20014027/demosimpleclick/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20014027/demosimpleclick/MainActivity.java
@@ -3,16 +3,22 @@ package sg.edu.rp.c346.id20014027.demosimpleclick;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tvDisplay;
     Button btnDisplay;
     EditText etInput;
+    RadioGroup rgGender;
+    ToggleButton toggleBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +28,36 @@ public class MainActivity extends AppCompatActivity {
         tvDisplay = findViewById(R.id.textViewDisplay);
         btnDisplay = findViewById(R.id.buttonDisplay);
         etInput = findViewById(R.id.editTextInput);
+        rgGender = findViewById(R.id.radioGroupGender);
+        toggleBtn = findViewById(R.id.toggleButtonEnabled);
 
         btnDisplay.setOnClickListener(new View.OnClickListener() {
             @Override
+            public void onClick(View v) {
+                // Code for the action
+                String stringResponse = etInput.getText().toString();
+                int checkedRadioId = rgGender.getCheckedRadioButtonId();
+                if(checkedRadioId == R.id.radioButtonGenderMale){
+                    // Write the code when male selected
+                    stringResponse = "He says " + stringResponse;
+                }
+                else{
+                    // Write the code when female selected
+                    stringResponse = "She says " + stringResponse;
+                }
+                tvDisplay.setText(stringResponse);
+            }
+        });
+
+        toggleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View view) {
-                String value = etInput.getText().toString();
-                tvDisplay.setText(value);
+                if(toggleBtn.isChecked()){
+                    etInput.setEnabled(true);
+                }
+                else{
+                    etInput.setEnabled(false);
+                }
             }
         });
     }
